0495d0a9f2dd3525f0808d21860169f95e4d9df5
Jamie <jamieburrows88@gmail.com>
2018-01-10 09:52:15 +1300

    First Commit

---
+++ Calculations.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author 92017403
+ */
+public class Calculations{
+    
+    public double total1 = 0.0;
+
+    
+}
---
+++ Launcher.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author 92017403
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorFrame frame = new CalculatorFrame();
+        frame.setVisible(true);
+    }
+    
+}
